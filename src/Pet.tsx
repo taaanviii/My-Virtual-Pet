6ed589a5280f8c897e450a872d99a1b1819d0ad0
taaanviii <tanvisingh2004@gmail.comm>
2025-02-26 01:00:41 +0530

    added different moods

--- src/Pet.tsx
+++ src/Pet.tsx
@@ -1,16 +1,39 @@
 import React, {useState} from 'react'
 import normal_pet from "./assets/Cat Animation loop.gif"
+import sleepy from "./assets/Snoozing Cat.gif"
+import hungry from "./assets/hungry cats Sticker by Platonic Games - Find & Share on GIPHY.gif"
+import happy from "./assets/Happy Good Vibes Sticker by Mino Games - Find & Share on GIPHY.gif"
+import sad from "./assets/sad.gif"
 
 function Pet() {
+  const [petImage, setPetImage] = useState(normal_pet)
+
+  const happyClick = () => {
+    setPetImage(happy)
+  }
+
+  const sadClick = () => {
+    setPetImage(sad)
+  }
+
+  const hungryClick = () => {
+    setPetImage(hungry)
+  }
+
+  const sleepyClick = () => {
+    setPetImage(sleepy)
+  }
+
   return (
+   
     <>
     <div>
-        <img src={normal_pet} alt="pet in normal position"></img>
+        <img src={petImage} alt="pet in normal position"></img>
         <div>
-            <button>Happy</button>
-            <button>Sad</button>
-            <button>Feed</button>
-            <button>Sleep</button>
+            <button onClick={happyClick}>Happy</button>
+            <button onClick={sadClick}>Sad</button>
+            <button onClick={hungryClick}>Feed</button>
+            <button onClick={sleepyClick}>Sleep</button>
         </div>
     </div>
     </>
