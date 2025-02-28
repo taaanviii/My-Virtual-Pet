d667149addbf81ea2ac45b3872aaa853c5dfb295
taaanviii <tanvisingh2004@gmail.comm>
2025-02-28 23:58:12 +0530

    added hearts on cursor movement

--- src/Pages/Pet.tsx
+++ src/Pages/Pet.tsx
@@ -1,15 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import "../Styles/Pet.css"
 import normal_pet from "../assets/Normal-cat.gif"
 import sleepy from "../assets/Snoozing-Cat-unscreen.gif"
 import hungry from "../assets/hungry cats Sticker by Platonic Games - Find & Share on GIPHY.gif"
 import happy from "../assets/Happy Good Vibes Sticker by Mino Games - Find & Share on GIPHY.gif"
 import sad from "../assets/sad.gif"
+import hearts from "../assets/hearts.png"
 
 function Pet() {
-  const [petImage, setPetImage] = useState(normal_pet)
-  const [petName, setPetName] = useState<string>('')
-  const [submittedName, setSubmittedName] = useState<string>('')
+  const [petImage, setPetImage] = useState(normal_pet);
+  const [petName, setPetName] = useState<string>('');
+  const [submittedName, setSubmittedName] = useState<string>('');
+
+  const [heartsVisible, setHeartsVisible] = useState(false); // To control heart visibility
+  const [hovering, setHovering] = useState(false); // To track if the mouse is hovering
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null); // Fix: Use ReturnType instead of NodeJS.Timeout
+  const divRef = useRef<HTMLDivElement | null>(null); // Reference to the div being hovered
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPetName(event.target.value)
@@ -45,9 +51,42 @@ function Pet() {
     setPetImage(sleepy)
   }
 
+  // Handler to start the timer when the mouse enters the div
+  const handleMouseEnter = () => {
+    setHovering(true);
+  };
+
+  // Handler to stop the timer when the mouse leaves the div
+  const handleMouseLeave = () => {
+    setHovering(false);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    setHeartsVisible(false);
+  };
+
+  // Check if the mouse is hovering for more than 3 seconds
+  useEffect(() => {
+    if (hovering) {
+      timeoutRef.current = setTimeout(() => {
+        setHeartsVisible(true); // Show hearts after 3 seconds
+      }, 1000); // 1 seconds
+    }
+
+    // Cleanup timeout if hovering stops before 3 seconds
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [hovering]);
+
   return (
    
-    <>
+    <div   ref={divRef}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    className="pet_page">
     <div className='pet_page'>
       {submittedName ? (
        <div className='name_header'> {submittedName && <h2>{submittedName}</h2>} </div>
@@ -60,8 +99,8 @@ function Pet() {
       </div>
       )
     }
-      
-    <div className='render_pet_image'>
+
+      <div className='render_pet_image'>
         <img src={petImage} alt="pet in normal position"></img>
         </div>
         <div>
@@ -70,8 +109,17 @@ function Pet() {
             <button onClick={hungryClick} className='mood_button'>FEED</button>
             <button onClick={sleepyClick} className='mood_button'>SLEEP</button>
             </div>
+
+            {heartsVisible && (
+        <div className="hearts-container">
+          <img src={hearts} alt="Heart" className="hearts" />
+          <img src={hearts} alt="Heart" className="hearts" />
+          <img src={hearts} alt="Heart" className="hearts" />
+        </div>
+      )}
+
     </div>
-    </>
+    </div>
   )
 }
 
